fd1fb9e3d7f188d355b37fbce128d9a72dfd0327
unknown <Mchughr97@gmail.com>
2022-04-27 01:48:23 +0100

    Jupyter notebook added

--- Iris.py
+++ Iris.py
@@ -1,9 +1,18 @@
 import csv
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df = pd.read_csv('Desktop/iris.csv')
-print(df.to_string()) 
-df = pd.DataFrame(data)
+df = pd.read_csv('desktop\iris.csv')
+print(df.to_string())
+df.describe()
 
-print(df.min())
-print(df.max())
+sum_df = df["SepalLengthCm"].sum()
+mean_df = df["SepalLengthCm"].mean()
+median_df = df["SepalLengthCm"].median()
+
+df.loc[df["Species"] == "Setosa"]
+
+df.plot(kind ="scatter",
+          x ='SepalLengthCm',
+          y ='PetalLengthCm')
+plt.grid()
